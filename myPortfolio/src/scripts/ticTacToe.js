76b0b676007fac723f12dbdcb2d5767c494c5c83
tcampbe <tcampb@icloud.com>
2019-06-06 23:35:45 -0500

    tweak some uglies

---
+++ myPortfolio/src/scripts/ticTacToe.js
@@ -0,0 +1,40 @@
+
+
+function myFunction(id) {
+    document.getElementById(id)
+    var=marker;
+
+    document.innerHTML = "marker";
+}
+
+// function myFunction() {
+//  var x = document.getElementsByClassName("example");
+//  x[0].innerHTML = "Hello World!";
+// }
+
+var mark;
+
+function omark(var mark=0) {
+  
+}
+
+function xmark(var mark=1) {
+
+}
+
+//  if (time < 20) {
+//    greeting = "Good day";
+//  } else {
+//    greeting = "Good evening";
+//  }
+
+
+
+
+//  if x is clicked then the value of "demo" is 1
+
+//                                    demo==1
+
+//  if o is clicked then the value of "demo" is 0
+
+//                                    demo==0
