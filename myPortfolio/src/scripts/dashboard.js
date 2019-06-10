703b5e63d555a7aa8ea73f63e24e4aac95a962f3
tcampbe <tcampb@icloud.com>
2019-06-09 19:59:04 -0500

    a little cleaning on dashboard

--- myPortfolio/src/scripts/dashboard.js
+++ myPortfolio/src/scripts/dashboard.js
@@ -1,40 +1,30 @@
-// $(document).ready(function() {
-
-//.....below is short for - the same as above....//
 $(function() {
-
-//..let locks in a var...$list is a variable (like x) .....\/
-//.............$('ul') is get the ul element.... 
-    let $list = $('ul');
-//...$('#newItemForm') is get the element with the id newItemForm ..\/
-//...id="newItemForm" in html is <form> element.......//
-    let $newItemForm = $('#newItemForm');
-  
-//.. $newItemForm.on is "on event" in this case the event is submit.\/
-//.. sumbit is the event .. function(e) is function(event)...//
-    $newItemForm.on('submit', function(e) {
-//.. e. is event prevent default prevnts the event form normal...\/
-//...in this case the event is submit on a form element...//
-      e.preventDefault();
-//....create variable 'text' .. = .. \/
-//.. like Javascript ...  <form text: <input type="text">
-//..this is onsubmit function (above)..??..<input type="submit"..?? 
-      let text = $('input[type="text"]').val();
-      $list.append(`<li>${text}</li>`); //add text to the list
-      $('input[type="text"]').val(''); // the input value is '' ??
+                         //..let locks in a var...$list is a variable (like x) .....\/
+    let $list = $('ul');//.............$('ul') is get the ul element....
+                                          //...$('#newItemForm') is get the element with the id newItemForm ..\/
+    let $newItemForm = $('#newItemForm');//...id="newItemForm" in html is <form> element.......//
+                                            //.. $newItemForm.on is "on event" in this case the event is submit.\/
+    $newItemForm.on('submit', function(e) {//.. sumbit is the event .. function(e) is function(event)...//
+                          //.. e. is event prevent default prevnts the event form normal...\/
+      e.preventDefault();//...in this case the event is submit on a form element...//
+                                                 //....create variable 'text' . \/
+                                                //.. like Javascript ...  <form text: <input type="text">
+      let text = $('input[type="text"]').val();//..variable text = what is typed in..// 
+      $list.append(`<li>${text}</li>`); //add text to the list..//
+      $('input[type="text"]').val(''); // the typed text..//
     });
-  
-    //.....this function removes a list item when clicked......//
-    $list.on('click', 'li', function() {
+    $list.on('click', 'li', function() {//.....this function removes a list item when clicked......//
       let $this = $(this);
       $this.remove();
     });
  
+  
     
 
-  });
+    
+
+});
 
 
 
   
-// }
